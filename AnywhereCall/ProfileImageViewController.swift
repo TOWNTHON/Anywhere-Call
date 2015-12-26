523292e81976a04280d1a36c0a1ccfb30bc523f5
GreenTea004 <midori253@gmail.com>
2015-12-26 16:36:50 +0900

    add to select profile image view

---
+++ AnywhereCall/ProfileImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileImageViewController.swift
+//  AnywhereCall
+//
+//  Created by 広瀬緑 on 12/26/15.
+//  Copyright © 2015 広瀬緑. All rights reserved.
+//
+
+import UIKit
+
+class ProfileImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
