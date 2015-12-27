3741afbe3bc01f838b8f5782ea6f2e8900d03581
GreenTea004 <midori253@gmail.com>
2015-12-27 12:26:26 +0900

    add QuartzCore

--- AnywhereCall/MosaicViewController.swift
+++ AnywhereCall/MosaicViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 import CoreImage
+import QuartzCore
 
 class MosaicViewController: UIViewController {
 
     // ベース画像.
-    let myInputImage = CIImage(image: UIImage(named: "icon-1024.png")!)
+    let myInputImage = CIImage(image: UIImage(named: "woman-profile-image.jpg")!)
     var myImageView: UIImageView!
     
     override func viewDidLoad() {
@@ -21,29 +22,32 @@ class MosaicViewController: UIViewController {
         let myInputUIImage: UIImage = UIImage(CIImage: myInputImage!)
         
         // ImageView.
-        myImageView = UIImageView(frame: CGRectMake(0, 0, myInputUIImage.size.width, myInputUIImage.size.height))
-        
-        // UIImageViewの生成.
+        myImageView = UIImageView(frame: CGRectMake(0, 0, 400, 640))
         myImageView.image = myInputUIImage
+        myImageView.layer.shouldRasterize = true
+        myImageView.layer.rasterizationScale = 0.1
+        myImageView.layer.minificationFilter = kCAFilterTrilinear
+        myImageView.layer.magnificationFilter = kCAFilterNearest
+        
         self.view.addSubview(myImageView)
         
-        // CIFilterを生成。nameにどんなを処理するのか記入.
-        let myPixellateFilter = CIFilter(name: "CIPixellate")
-        
-        // ぼかし処理をいれたい画像をセット.
-        myPixellateFilter!.setValue(myInputImage, forKey: kCIInputImageKey)
-        
-        // フィルターを通した画像をアウトプット.
-        let myOutputImage : CIImage = myPixellateFilter!.outputImage!
-        
-        // UIImageに変換.
-        let myOutputUIImage: UIImage = UIImage(CIImage: myOutputImage)
-        
-        // 再びUIViewにセット.
-        myImageView.image = myOutputUIImage
-        
-        // 再描画.
-        myImageView.setNeedsDisplay()
+//        // CIFilterを生成。nameにどんなを処理するのか記入.
+//        let myPixellateFilter = CIFilter(name: "CIPixellate")
+//        
+//        // ぼかし処理をいれたい画像をセット.
+//        myPixellateFilter!.setValue(myInputImage, forKey: kCIInputImageKey)
+//        
+//        // フィルターを通した画像をアウトプット.
+//        let myOutputImage : CIImage = myPixellateFilter!.outputImage!
+//        
+//        // UIImageに変換.
+//        let myOutputUIImage: UIImage = UIImage(CIImage: myOutputImage)
+//        
+//        // 再びUIViewにセット
+//        myImageView.image = myOutputUIImage
+//        
+//        // 再描画.
+//        myImageView.setNeedsDisplay()
 
     }
     
